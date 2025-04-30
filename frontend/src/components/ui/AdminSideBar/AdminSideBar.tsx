45f0e9b32cab019e0bba996aec6b4796ca8b610b
Enzo_Ro_Haus <105392239+Enzo-Ro-Haus@users.noreply.github.com>
2025-04-30 12:17:47 +0000

    Agregado de clases

--- frontend/src/components/ui/AdminSideBar/AdminSideBar.tsx
+++ frontend/src/components/ui/AdminSideBar/AdminSideBar.tsx
@@ -4,8 +4,8 @@ import style from "./AdminSideBar.module.css";
 export const AdminSideBar = () => {
   return (
     <div className={style.containerSideBar}>
-        <h2>Admin</h2>
-        <h3>Products/TSHIRT</h3>
+        <h2 className={style.containerSideBarTitle}>Admin</h2>
+        <h3 className={style.containerSideBarSubTitle}>Products/TSHIRT</h3>
       <div>
         <button>T-SHIRT</button>
       </div>

c57f8fdc6706964894a76deb8ace91e2dfab826d
Enzo-Ro-Haus <enzo.ro.haus@gmail.com>
2025-06-10 18:14:49 -0300

    Login y showelement

--- frontend/src/components/ui/Showcase/Showcase.tsx
+++ frontend/src/components/ui/Showcase/Showcase.tsx
@@ -1,23 +1,38 @@
-import { useEffect } from "react";
-import { getAllProductos } from "../../../http/productoHTTP";
+
 import { ViewAllButton } from "../Buttons/ViewAllButton/ViewAllButton";
 import { ClotheCard } from "../Card/ClotheCard/ClotheCard";
 
 import styles from "./Showcase.module.css";
 import { productoStore } from "../../../store/prodcutoStore";
+const productos = productoStore((s) => s.productos);
+
+//Puede que falte el useEffect
 
 export const Showcase = () => {
-
   return (
     <div className={styles.containerPincipalShowcase}>
       <div className={styles.containerTitleButton}>
-        <h2 className={styles.containerTitle}>Catalog</h2>
+        <h2 className={styles.containerTitle}>CATALOG</h2>
         <div className={styles.buttonContainer}>
           <ViewAllButton />
         </div>
       </div>
       <div className={styles.containerClotheCards}>
-        
+        {productos.length > 0 ? (
+          productos
+            .slice(0, 4)
+            .map((el) => (
+              <ClotheCard
+                name={el.nombre}
+                description={el.descripcion}
+                price={el.precio}
+              />
+            ))
+        ) : (
+          <div>
+            <h3>No hay productos</h3>
+          </div>
+        )}
       </div>
     </div>
   );
